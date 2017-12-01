380ea20fc96b596454b46f1f33c86507cc848345
Mads <madsbaan@gmail.com>
2017-12-01 14:12:52 +0100

    Define pins

--- master-arduino/master-arduino.ino
+++ master-arduino/master-arduino.ino
@@ -1,6 +1,10 @@
-void setup() {
-  // put your setup code here, to run once:
+#define pinSwitch 3
+#define pinPotentiometer A0
+#define pinKnap 4
+#define pinGreenLED 6
+#define pinRedLED 5
 
+void setup() {
 }
 
 void loop() {
