8392d61259a7260f419f4dbaeb43f1cf807a9503
Mads <madsbaan@gmail.com>
2017-12-12 10:52:55 +0100

    Add code

--- slave-arduino/slave-arduino.ino
+++ slave-arduino/slave-arduino.ino
@@ -1,9 +1,53 @@
-void setup() {
-  // put your setup code here, to run once:
+#define pinKaffe 12
+#define pinKaffeOgVarme 11
+#define pinDebug 13
+#define pinKnap 7
 
+bool kaffeState = false;
+
+void setup() {
+  pinMode(pinKaffe, OUTPUT);
+  pinMode(pinKaffeOgVarme, OUTPUT);
+  pinMode(pinKnap, INPUT_PULLUP);
+  pinMode(pinDebug, OUTPUT);
+
+  Serial.begin(9600);
+
+  slukKaffemaskine();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int knapTryk = !digitalRead(pinKnap);
+  if(knapTryk) {
+    Serial.println("ON!");
+    lavKaffeMedVarme();
+    float delayTime = 180000;
+    while(delayTime > 0) {
+      delay(1000);
+      delayTime -= 1000;
+      Serial.println(delayTime / 1000);
+    }
+    slukKaffemaskine();
+    Serial.println("OFF");
+  }
 }
+
+void lavKaffe() {
+  digitalWrite(pinKaffe, HIGH);
+  kaffeState = true;
+  digitalWrite(pinDebug, HIGH);
+}
+
+void lavKaffeMedVarme() {
+  digitalWrite(pinKaffeOgVarme, HIGH);
+  kaffeState = true;
+  digitalWrite(pinDebug, HIGH);
+}
+
+void slukKaffemaskine() {
+  digitalWrite(pinKaffe, LOW);
+  digitalWrite(pinKaffeOgVarme, LOW);
+  kaffeState = false;
+  digitalWrite(pinDebug, LOW);
+}
+
